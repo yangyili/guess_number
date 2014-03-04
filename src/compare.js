dd366adee1ed00c955b29eda3b353849929232f0
yangyili <1156612970@qq.com>
2014-03-04 13:39:10 +0800

    recontruct compare_function

--- src/compare.js
+++ src/compare.js
@@ -1,14 +1,6 @@
-function str_to_arr(str) {
-    var arr = [];
-    for(var i = 0; i < str.length; i++) {
-        arr[i] = str.substr(i, 1);
-    }
-    return arr;
-}
-
 function compare(val1, val2) {
-    var arr1 = str_to_arr(val1);
-    var arr2 = str_to_arr(val2);
+    var arr1 = val1.split("");
+    var arr2 = val2.split("");
     var A = 0;
     var B = 0;
     for(var i = 0; i < arr1.length; i++) {
