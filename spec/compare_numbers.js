ed2ea92b9f6b492dcad2cfe21fa65ccdb2e66ea6
yangyili <1156612970@qq.com>
2014-03-04 14:06:30 +0800

    recontruct test

--- spec/compare_numbers.js
+++ spec/compare_numbers.js
@@ -1,21 +1,17 @@
 describe("compare_numbers", function() {
-    it("should return 4A0B when compare '1234' and '1234'", function() {
+    it("should pass when compare all number is same", function() {
         expect(compare("1234", "1234")).toEqual("4A0B");
-    });
-
-    it("should return 0A4B when compare '1234' and '2341", function() {
         expect(compare("1234", "2341")).toEqual("0A4B");
     });
-
+    it("should return 0A4B when compare different numbers", function() {
+        expect(compare("1234", "5678")).toEqual("0A0B");
+    });
     it("should return 2A2B when compare '1234' and '1243", function() {
         expect(compare("1234", "1243")).toEqual("2A2B");
     });
-
-    it("should return 1A0B when compare '1234' and '1678", function() {
+    it("should pass when compare two numbers which has part same", function() {
         expect(compare("1234", "1678")).toEqual("1A0B");
-    });
-
-    it("should return 0A0B when compare '1234' and '5678", function() {
-        expect(compare("1234", "5678")).toEqual("0A0B");
+        expect(compare("1234", "1278")).toEqual("2A0B");
+        expect(compare("1234", "1230")).toEqual("3A0B");
     });
 });
