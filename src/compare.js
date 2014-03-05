bfd8ccb958f59049069204863606510f67127f4d
yangyili <1156612970@qq.com>
2014-03-05 23:53:30 +0800

    rename argument in compare function

--- src/compare.js
+++ src/compare.js
@@ -1,15 +1,15 @@
-function compare(val1, val2) {
-    var arr1 = val1.split("");
-    var arr2 = val2.split("");
-    var A = 0;
-    var B = 0;
+function compare(number1, number2) {
+    var arr1 = number1.toString().split("");
+    var arr2 = number2.toString().split("");
+    var a_count = 0;
+    var b_count = 0;
     for(var i = 0; i < arr1.length; i++) {
         for(var j = 0;j < arr2.length; j++) {
             if (arr1[i] == arr2[j]) {
                 if (i==j) {
-                    A++;
+                    a_count++;
                 } else {
-                    B++;
+                    b_count++;
                 }
             }
         }
