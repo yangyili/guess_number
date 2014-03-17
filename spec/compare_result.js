bf73f6d70295c49d4763c594d9fef2577d7a8cef
yangyili <1156612970@qq.com>
2014-03-17 14:15:22 +0800

    compare number

--- spec/compare_result.js
+++ spec/compare_result.js
@@ -4,6 +4,6 @@ describe("compare one given four_digit_number to a random", function() {
     });
 
     it("should return 4A0B compare '1234' to 'a random'", function() {
-        expect(guess_number(get_random(), '2341')).toEqual('0A4B');
+        expect(guess_number(get_random(), 2341)).toEqual('0A4B');
     });
 });
