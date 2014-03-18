bf21538af5f0c168ccae27729d4719bbc4afd381
yangyili <1156612970@qq.com>
2014-03-18 14:10:15 +0800

    reconstrucnt guess_result

--- spec/guess_number_spec.js
+++ spec/guess_number_spec.js
@@ -1,30 +1,18 @@
-describe('guess number', function() {
+describe('run guess number', function() {
     beforeEach(function() {
         spyOn(window, 'get_random').and.returnValue(1234);
     });
 
-    it('should remind user input again when previous input number has repeat', function() {
-        expect(is_input_number_has_repeat(1123)).toEqual(true);
+    it('remind user input again', function() {
+        expect(game_run(get_random, 1379)).toEqual('1A1B input again');
     });
 
-    it('should remind user input again when previous input first number is 0 repeat', function() {
-        expect(is_first_number_zero(0123)).toEqual(true);
+    it('', function() {
+        expect(game_run(get_random, [1379, 2345, 1345, 1234])).toEqual('win! game over');
     });
 
-    it('should response user win the game when user input number equal to random and guess times less than 7', function() {
-        expect(is_guess_number_win(random, 1234)).toEqual(true);
-        expect(guess_time()).toBeLessThan(7);
-    });
-
-    it('should response user lost the game when user input number not equal to random and guess times greater than 6', function() {
-        expect(is_guess_number_win(random, 1234)).toEqual(false);
-        expect(guess_time()).toBeGreaterThan(6);
-    });
-
-    it('should response guess_result and remind user input again when the previous number not equal random but guess times less than 7', function() {
-        expect(is_guess_number_win(random, 2341)).toEqual(false);
-        expect(guess_number(get_random(), 2341)).toEqual('0A4B');
-        expect(guess_time()).toBeLessThan(6);
+    it('', function() {
+        expect(game_run(get_random, [1379, 2345, 1345, 1534, 1536, 2389])).toEqual('lose! game over');
     });
 
 });
